4840bf4507aac1ff8b1f415a0f494dfe21a21463
VidhiChheda <67408078+VidhiChheda@users.noreply.github.com>
2022-12-19 13:43:07 +0530

    Create setup.py

---
+++ Vidhitoolbox/library3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='library3',
+    version='0.0.3',
+    author='Vidhi Chheda',
+    author_email='vidhi@swancapital.com',
+    description='Testing installation of Package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/VidhiChheda/VidhiPythonLibraries/Vidhitoolbox/library3',
+    project_urls = {
+        "Bug Tracker": "https://github.com/VidhiChheda/VidhiPythonLibraries/Vidhitoolbox/issues"},
+    #license='MIT',
+    packages=['library3'],
+    install_requires=['requests'],
+)
+
