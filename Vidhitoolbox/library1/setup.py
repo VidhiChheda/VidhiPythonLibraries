cd8bf0fd9e0f561ef2fbf845dccf54f961df326b
VidhiChheda <67408078+VidhiChheda@users.noreply.github.com>
2022-12-19 11:45:17 +0530

    Update setup.py

--- Vidhitoolbox/library1/setup.py
+++ Vidhitoolbox/library1/setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='library1',
+    name='packagecheck1',
     version='0.0.3',
     author='Vidhi Chheda',
     author_email='vidhi@swancapital.com',
     description='Testing installation of Package',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/VidhiChheda/VidhiPythonLibraries/Vidhitoolbox/library1',
+    url='https://github.com/VidhiChheda/VidhiPythonLibraries/Vidhitoolbox/library1/packagecheck1',
     #license='MIT',
-    packages=['toolbox'],
+    packages=['packagecheck1'],
     install_requires=['requests'],
 )
